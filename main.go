cedf08e28c1f9191f54032fea11706d9e9dfdebb
JMJ <yang.jmj@gmail.com>
2022-09-13 14:06:43 +0800

    update

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"learn-go-with-cli/cmd"
+	//_ "learn-go-with-cli/go_notes/volume1/summary"
+	//_ "learn-go-with-cli/go_notes/volume1/type"
 	_ "learn-go-with-cli/gobyexample_com"
 	_ "learn-go-with-cli/pointer"
 	_ "learn-go-with-cli/question"
